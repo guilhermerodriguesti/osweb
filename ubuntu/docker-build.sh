6216128c564ae3c64a22953cfa0e11877b45b35e
DevOps <infraestrutura.devops@gmail.com>
2021-01-13 12:17:48 -0300

    Add docker build

--- ubuntu/docker-build.sh
+++ ubuntu/docker-build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker build -t ubuntu-web Dockerfile.ubuntu
+IMAGE_NAME=$(basename "$PWD")
+TAG=`git rev-parse --short HEAD`
+echo "BUILD IMAGE AND PUSH"
+
+docker build -t "${IMAGE_NAME}:${TAG}" -t "${IMAGE_NAME}:latest" .
+docker push "${IMAGE_NAME}:${TAG}"
+docker push "${IMAGE_NAME}:latest"
+docker run -p 6080:6080 "${IMAGE_NAME}:latest"
